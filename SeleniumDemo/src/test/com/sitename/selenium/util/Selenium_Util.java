c6e3d7de9eeabf4f6c329745fe0aae3fbf0ab4a7
javaguy0726 <terai2012@gmail.com>
2013-01-31 01:00:59 +0800

    selenium_util  delete some import packages

--- SeleniumDemo/src/test/com/sitename/selenium/util/Selenium_Util.java
+++ SeleniumDemo/src/test/com/sitename/selenium/util/Selenium_Util.java
@@ -20,7 +20,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.interactions.Actions;
