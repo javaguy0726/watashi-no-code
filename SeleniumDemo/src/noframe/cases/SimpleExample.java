fec874a776e408337d7675c07729a6fc002f776e
renta <terai2012@gmail.com>
2013-02-04 10:36:01 +0800

    update simpleselenium.java

--- SeleniumDemo/src/noframe/cases/SimpleExample.java
+++ SeleniumDemo/src/noframe/cases/SimpleExample.java
@@ -14,34 +14,47 @@ import com.thoughtworks.selenium.SeleneseTestBase;
 
 public class SimpleExample {
 	public static void main(String[] args) {
-		
-		WebDriver driver=  new ChromeDriver();
-        driver.get("http://www.360buy.com/");
-        
-        (new WebDriverWait(driver, 20)).until(new ExpectedCondition<Boolean>() {
-            public Boolean apply(WebDriver d) {
-                return d.findElement(By.id("subscription-submit")).isDisplayed();
-            }
-        });
-       
-        WebElement input_search = driver.findElement(By.id("key"));
-        
-        WebElement button_search = driver.findElement(By.xpath("//*[@id='search']//input[@type='button']"));
+		System.setProperty("webdriver.chrome.driver",
+				"/home/renta/Tools/resouces/chromedriver");
+		WebDriver driver = null;
+		try {
 
-        input_search.sendKeys("12.12");
+			driver = new ChromeDriver();
+			driver.get("http://www.360buy.com/");
 
-        button_search.click();
+			(new WebDriverWait(driver, 20))
+					.until(new ExpectedCondition<Boolean>() {
+						public Boolean apply(WebDriver d) {
+							return d.findElement(By.id("subscription-submit"))
+									.isDisplayed();
+						}
+					});
 
-        (new WebDriverWait(driver, 20)).until(new ExpectedCondition<Boolean>() {
-            public Boolean apply(WebDriver d) {
-                return d.findElement(By.id("plist")).isDisplayed();
-            }
-        });
-        
-        List<WebElement> products_name = driver.findElements(By.xpath("//div[@id='plist']//li/div[2]"));
-        for(WebElement product: products_name){
-        	System.out.println(product.getText());
-        }
-        driver.quit();
+			WebElement input_search = driver.findElement(By.id("key"));
+
+			WebElement button_search = driver.findElement(By
+					.xpath("//*[@id='search']//input[@type='button']"));
+
+			input_search.sendKeys("12.12");
+
+			button_search.click();
+
+			(new WebDriverWait(driver, 20))
+					.until(new ExpectedCondition<Boolean>() {
+						public Boolean apply(WebDriver d) {
+							return d.findElement(By.id("plist")).isDisplayed();
+						}
+					});
+
+			List<WebElement> products_name = driver.findElements(By
+					.xpath("//div[@id='plist']//li/div[2]"));
+			for (WebElement product : products_name) {
+				System.out.println(product.getText());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			driver.quit();
+		}
 	}
 }
