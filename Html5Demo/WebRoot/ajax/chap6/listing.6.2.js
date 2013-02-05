8c889619561fabe979802329aa95d86802041250
renta <terai2012@gmail.com>
2013-02-06 00:17:04 +0800

    add ninja
    add comments to ajax 6.2

--- Html5Demo/WebRoot/ajax/chap6/listing.6.2.js
+++ Html5Demo/WebRoot/ajax/chap6/listing.6.2.js
@@ -1,38 +1,54 @@
 var Validator = Class.create();
 
 Validator.prototype = {
-	type: all,
-	initialize: function(validators){
-		validators[this.type] = this;//?爲什麼要這麼賦值
-	},
-	
-	doValidate: function(input){
-		return "";
-	},
-	
-	validate: function(input, errordiv){
-		errorMsg = this.doValidate(input);
-		errordiv.innerHTML= errorMsg;
-		return (errorMsg.length == 0);
-	}
-		
+  type: "all",
+
+  initialize: function(validators) {                          
+    validators[this.type] = this;  //  此方法被子类继承，当new本类的时候 会将传进来的validators对象进行设置值指向本引用;
+  },
+
+  doValidate: function(input) {                              
+    return "";
+  },
+
+  validate: function(input, errordiv) {                       
+    errorMsg = this.doValidate(input);
+    errordiv.innerHTML = errorMsg;
+    return (errorMsg.length == 0);  //如果为空串 那么就是true
+  }
 };
+
+var NumberValidator = Class.create();   //扩展性良好
+
+Object.extend(NumberValidator.prototype,Validator.prototype); //先将Validator的内容给继承了,
+
+Object.extend(NumberValidator.prototype, {			//然后再声明属于自己的一些属性和方法
+    type: "number",                                           
+
+     doValidate: function(input) {                            
+      var numberpattern=/(^\d+$)|(^\d+\.\d+$)/;
+      if (numberpattern.test(input)) {
+        return "";
+      } else {
+        return "'" + input + "' is not a number." ;
+      }
+    }
+});
 
 var ValidatorFramework = Class.create();
 
 ValidatorFramework.prototype =
 {
-  validators: 0,                                             //#6
-
-  initialize: function() {                                    //#9
+  validators: 0, 
+  
+  initialize: function() {                     //用来初始话validators map对象的值的               
 	    this.validators = new Object();
-	    new Validator(this.validators);
+	    new Validator(this.validators);   //传的是引用，所以值可以保存下来;
 	    new NumberValidator(this.validators);
 	  },
-  
+	  
   validateForm: function(form) {
     var retval = true;
-   
     for(var i = 0; i < form.length; i++) {
       currentInput = form[i];
       type = currentInput.getAttribute("valid");
@@ -40,7 +56,7 @@ ValidatorFramework.prototype =
       if(type == null || errorDivName == null) {
         continue;
       } else {
-        valid = this.validate(                                //#7
+        valid = this.validate(                                
           type, currentInput.value, $(errorDivName));
         if(!valid) {
           retval = false;
@@ -51,26 +67,9 @@ ValidatorFramework.prototype =
   },
 
   validate: function(type, input, errordiv) {
-    return this.validators[type].                             //#8
-      validate(input, errordiv);                              //#8
+	  return this.validators[type].validate(input, errordiv);                              
   }
-};
 
+ 
 
-var NumberValidator = Class.create();
-
-Object.extend(NumberValidator.prototype,
-              Validator.prototype);
-
-Object.extend(NumberValidator.prototype, {
-    type: "number",                                           //#4
-
-     doValidate: function(input) {                            //#5
-      var numberpattern=/(^\d+$)|(^\d+\.\d+$)/;
-      if (numberpattern.test(input)) {
-        return "";
-      } else {
-        return "'" + input + "' is not a number." ;
-      }
-    }
-});
+}
