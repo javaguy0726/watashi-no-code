33b1e92dd304a6b310dfb7e966010aa1548d3e9d
renta <terai2012@gmail.com>
2013-02-05 18:19:47 +0800

    ajax examples made some changes

--- Html5Demo/src/org/aip/chap5/ParameterInspectorServlet.java
+++ Html5Demo/src/org/aip/chap5/ParameterInspectorServlet.java
@@ -2,6 +2,8 @@ package org.aip.chap5;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 import java.util.TreeMap;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,19 +11,24 @@ import javax.servlet.http.HttpServletResponse;
 
 public class ParameterInspectorServlet extends HttpServlet {
 
-  protected void doPost( HttpServletRequest request,
-                         HttpServletResponse response )
-        throws IOException {
-    StringBuilder result = new StringBuilder();
-    Map<String,String[]> entries = new TreeMap();
-    entries.putAll(request.getParameterMap());
-    for (Map.Entry<String,String[]> entry : entries.entrySet()) {
-      result.append(entry.getKey()).append("=<br/>");
-      for (String value : entry.getValue()) {
-        result.append("&nbsp;").append(value).append("<br/>");
-      }
-    }
-    response.getWriter().write(result.toString());
-  }
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws IOException {
 
+		StringBuilder result = new StringBuilder();
+
+		Map<String, String[]> requestMap = new TreeMap<String, String[]>();
+
+		requestMap.putAll(request.getParameterMap());// request.getParameterMap()得到的map是不可改的;
+
+		Set<Entry<String, String[]>> entries = requestMap.entrySet();
+
+		for (Entry<String, String[]> entry : entries) {
+			result.append(entry.getKey()).append("=<br/>");
+			for (String value : entry.getValue()) {
+				result.append("<strong>" + value + "</strong>").append("<br/>");
+			}
+		}
+
+		response.getWriter().write(result.toString());
+	}
 }
