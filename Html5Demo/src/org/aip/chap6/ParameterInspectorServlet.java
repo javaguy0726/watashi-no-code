63ed7d186c17b80a30f1ba1b4f9f67e7b3dc1f60
renta <terai2012@gmail.com>
2013-02-06 15:29:12 +0800

    jquery...

--- Html5Demo/src/org/aip/chap6/ParameterInspectorServlet.java
+++ Html5Demo/src/org/aip/chap6/ParameterInspectorServlet.java
@@ -9,20 +9,28 @@ import javax.servlet.http.HttpServletResponse;
 
 public class ParameterInspectorServlet extends HttpServlet {
 
-  protected void doPost( HttpServletRequest request,
-                         HttpServletResponse response )
-        throws IOException {
-    StringBuilder result = new StringBuilder();
-    Map<String,String[]> entries = new TreeMap();
-    entries.putAll(request.getParameterMap());
-    for (Map.Entry<String,String[]> entry : entries.entrySet()) {
-      result.append(entry.getKey()).append("=<br/>");
-      for (String value : entry.getValue()) {
-        result.append("&nbsp;").append(value).append("<br/>");
-      }
-    }
-    response.setContentType( "text/html" );
-    response.getWriter().write(result.toString());
-  }
+	protected void doPost(HttpServletRequest request,HttpServletResponse response){
+		StringBuilder result = new StringBuilder();
+		Map<String, String[]> entries = new TreeMap<String, String[]>();
+		entries.putAll(request.getParameterMap());
+		for (Map.Entry<String, String[]> entry : entries.entrySet()) {
+			result.append(entry.getKey()).append("=<br/>");
+			for (String value : entry.getValue()) {
+				result.append("&nbsp;").append(value).append("<br/>");
+			}
+		}
+		response.setContentType("text/html");
+		try {
+			response.getWriter().write(result.toString());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
+	protected void doGet(HttpServletRequest request,HttpServletResponse response){
+		
+		System.out.println("Get");
+		this.doPost(request, response);
+	}
 }
