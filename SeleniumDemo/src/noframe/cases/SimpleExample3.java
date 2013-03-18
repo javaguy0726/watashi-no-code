1d34dcaa561edb90ad95ed47ebc8ce7766786848
javaguy0726 <terai2012@gmail.com>
2013-03-18 15:08:15 +0800

    make no sense

--- SeleniumDemo/src/noframe/cases/SimpleExample3.java
+++ SeleniumDemo/src/noframe/cases/SimpleExample3.java
@@ -16,11 +16,22 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
 
-public class SimpleExample3 implements Serializable {
+class MyDriver extends ChromeDriver implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+}
+
+public class SimpleExample3{
 	private WebDriver driver;
-	private JavascriptExecutor js;
 	private WebDriver driver2;
-	public SimpleExample3(RemoteWebDriver driver) {
+	private JavascriptExecutor js1;
+	private JavascriptExecutor js2;
+
+	public SimpleExample3(WebDriver driver) {
 		this.driver = driver;
 	}
 
@@ -33,54 +44,68 @@ public class SimpleExample3 implements Serializable {
 		SimpleExample3 example2 = null;
 		try {
 			setSystemProperties();
-			example2 = new SimpleExample3(new ChromeDriver());
-			example2.js = (JavascriptExecutor) example2.driver;
+			example2 = new SimpleExample3(new MyDriver());
+			example2.js1 = (JavascriptExecutor) example2.driver;
 			example2.driver.get("http://www.baidu.com/");
+
 			URL jqueryUrl = Resources.getResource("jquery-1.8.3.js");
 			String jqueryText = Resources.toString(jqueryUrl, Charsets.UTF_8);
-			example2.js.executeScript(jqueryText);
+			example2.js1.executeScript(jqueryText);
+
 			example2.driver2 = example2.clone(example2.driver);
-			System.out.println(example2.driver2==example2.driver);
+			System.out.println(example2.driver2 == example2.driver);
 			
-			example2.js = (JavascriptExecutor) example2.driver2;
 			
+			example2.js1 = (JavascriptExecutor) example2.driver2;
 			String jsript = "return $(\"#kw\").val('aaaaaaaaaa')";
-			example2.js.executeScript(jsript);
+			example2.js1.executeScript(jsript);
 			Thread.sleep(5000);
+			
+			example2.js2 = (JavascriptExecutor) example2.driver2;
+			String jsript2 = "return $(\"#kw\").val('bbbbbbbbbb')";
+			example2.js2.executeScript(jsript);
+			Thread.sleep(5000);
+			
+			
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			example2.driver.quit();
 		}
 	}
-	
-	 private WebDriver clone(WebDriver driver) {   
-	        ByteArrayOutputStream byteOut = null;   
-	        ObjectOutputStream objOut = null;   
-	        ByteArrayInputStream byteIn = null;   
-	        ObjectInputStream objIn = null;   
-	           
-	        try {   
-	            byteOut = new ByteArrayOutputStream();    
-	            objOut = new ObjectOutputStream(byteOut);    
-	            objOut.writeObject(driver);   
-	  
-	            byteIn = new ByteArrayInputStream(byteOut.toByteArray());   
-	            objIn = new ObjectInputStream(byteIn);   
-	               
-	            return (WebDriver) objIn.readObject();   
-	        } catch (IOException e) {   
-	            throw new RuntimeException("Clone Object failed in IO.",e);      
-	        } catch (ClassNotFoundException e) {   
-	            throw new RuntimeException("Class not found.",e);      
-	        } finally{   
-	            try{   
-	                byteIn = null;   
-	                byteOut = null;   
-	                if(objOut != null) objOut.close();      
-	                if(objIn != null) objIn.close();      
-	            }catch(IOException e){      
-	            }      
-	        }   
-	    }   
+
+	public MyDriver clone(WebDriver driver) {
+		
+		ByteArrayOutputStream byteOut = null;
+		ObjectOutputStream objOut = null;
+		ByteArrayInputStream byteIn = null;
+		ObjectInputStream objIn = null;
+
+		try {
+			byteOut = new ByteArrayOutputStream();
+			objOut = new ObjectOutputStream(byteOut);
+			objOut.writeObject(driver);
+
+			byteIn = new ByteArrayInputStream(byteOut.toByteArray());
+			objIn = new ObjectInputStream(byteIn);
+
+			return (MyDriver)objIn.readObject();
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Clone Object failed in IO.", e);
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Class not found.", e);
+		} finally {
+			try {
+				byteIn = null;
+				byteOut = null;
+				if (objOut != null)
+					objOut.close();
+				if (objIn != null)
+					objIn.close();
+			} catch (IOException e) {
+			}
+		}
+	}
 }
